818a880a511524409815e3e1b670cea0d23f44b0
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-02-17 13:54:15 +0100

    Canvas scrolable.

--- interface.py
+++ interface.py
@@ -9,7 +9,7 @@ class MainWindow:
     def __init__(self):
         self.root = Tk()
         self.root.title('Python POC')  # Nom de la fenêtre
-        self.root.geometry("400x200")  # Taille de la fenêtre
+        self.root.geometry("800x600")  # Taille de la fenêtre
         self.root.resizable(False, False)  # Ne pas redimensionner fenêtre
 
         # Objet représentant la DB
@@ -20,21 +20,27 @@ class MainWindow:
         self.nomDb.set("Connection à la base de données {}".format(self.dB.getName()))
         self.nomTable = StringVar()
 
-
-
-
         # Interface graphique
         self.l_nomDB = Label(self.root, textvariable=self.nomDb)
-        self.l_nomDB.place(anchor='nw', width=400, height=50, x=0, y=10)
+        self.l_nomDB.place(anchor='n', width=800, height=50, x=400, y=10)
 
         self.e_nomTable = Entry(self.root, textvariable=self.nomTable, background="white", justify="center")
-        self.e_nomTable.place(anchor='center', width=300, height=35, x=200, y=85)
+        self.e_nomTable.place(anchor='center', width=600, height=35, x=400, y=85)
 
         self.b_chercherTable = Button(self.root, text="Voir contenue de la table", command=self.b_affichage)
-        self.b_chercherTable.place(anchor='n', width=200, height=35, x=200, y=150)
+        self.b_chercherTable.place(anchor='n', width=250, height=35, x=400, y=110)
 
         self.l_tableFausse = Label(self.root, text="La table que tu m'as donné ne marche pas ...", fg='red')
         # self.l_tableFausse.place(anchor='n', width=250, height=35, x=400, y=150)
+
+        # self.f_res = Frame(self.root, background="white", highlightbackground="black", bd=2)
+        self.f_results = VerticalScrolledFrame(self.root)
+        self.f_results.place(anchor="n", width=700, height=400, x=400, y=190)
+
+        buttons = []
+        for i in range(20):
+            buttons.append(ButtonElDB(self.f_results.interior, i).getBt())
+            buttons[-1].pack()
 
         self.dB.connect()
 
@@ -43,11 +49,8 @@ class MainWindow:
     def b_affichage(self):
         exist = self.dB.existanceTable(self.nomTable.get())
 
-        # ctn = self.dB.contenuTable(self.nomTable.get())
-        # print(ctn)
-
         if not exist:
-            self.l_tableFausse.place(anchor='n', width=250, height=35, x=200, y=110)
+            self.l_tableFausse.place(anchor='n', width=250, height=35, x=400, y=150)
             self.nomTable.set("")
         else:
             self.l_tableFausse.place_forget()
@@ -58,29 +61,42 @@ class ButtonElDB:
         self.root = root
 
         self.bt = Button(self.root, text=t, command=self.fermer)
+
         self.bt.pack()
 
     def fermer(self):
         self.bt.pack_forget()
 
+    def getBt(self):
+        return self.bt
 
-class ScrollableCanvas(Frame):
+
+class VerticalScrolledFrame(Frame):
+    """A pure Tkinter scrollable frame that actually works!
+    * Use the 'interior' attribute to place widgets inside the scrollable frame
+    * Construct and pack/place/grid normally
+    * This frame only allows vertical scrolling
+
+    """
     def __init__(self, parent, *args, **kw):
         Frame.__init__(self, parent, *args, **kw)
 
-        canvas = Canvas(self, bg='#FFFFFF', width=300, height=300, scrollregion=(0, 0, 500, 500))
+        # create a canvas object and a vertical scrollbar for scrolling it
+        vscrollbar = Scrollbar(self, orient=VERTICAL)
+        vscrollbar.pack(fill=Y, side=RIGHT, expand=FALSE)
+        canvas = Canvas(self, bd=0, highlightthickness=0,
+                        yscrollcommand=vscrollbar.set)
+        canvas.pack(side=LEFT, fill=BOTH, expand=TRUE)
+        vscrollbar.config(command=canvas.yview)
 
-        vbar = Scrollbar(self, orient=VERTICAL)
-        vbar.pack(side=RIGHT, fill=Y)
-        vbar.config(command=canvas.yview)
-
-        canvas.config(width=200, height=200)
-        canvas.config(yscrollcommand=vbar.set)
-        canvas.pack(side=LEFT, expand=True, fill=BOTH)
+        # reset the view
+        canvas.xview_moveto(0)
+        canvas.yview_moveto(0)
 
         # create a frame inside the canvas which will be scrolled with it
         self.interior = interior = Frame(canvas)
-        interior_id = canvas.create_window(0, 0, window=interior, anchor=NW)
+        interior_id = canvas.create_window(0, 0, window=interior,
+                                           anchor=NW)
 
         # track changes to the canvas and frame width and sync them,
         # also updating the scrollbar
@@ -91,12 +107,10 @@ class ScrollableCanvas(Frame):
             if interior.winfo_reqwidth() != canvas.winfo_width():
                 # update the canvas's width to fit the inner frame
                 canvas.config(width=interior.winfo_reqwidth())
-
         interior.bind('<Configure>', _configure_interior)
 
         def _configure_canvas(event):
             if interior.winfo_reqwidth() != canvas.winfo_width():
                 # update the inner frame's width to fill the canvas
                 canvas.itemconfigure(interior_id, width=canvas.winfo_width())
-
         canvas.bind('<Configure>', _configure_canvas)
