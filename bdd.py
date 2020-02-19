de2396dd99903488cbf1e5016de83a5807e5aaec
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-02-19 17:23:45 +0100

    Ajout de méthodes pour supprimer, ajouter et modifier

--- bdd.py
+++ bdd.py
@@ -33,9 +33,16 @@ class DbClass:
             print("[DATABASE] Connected")
 
         except:
+            print("[DATABASE] Fail connection")
             self.connect()
 
+    def deco(self):
+        self.con.close()
+        print("[DATABASE] Disconected")
+
     def existanceTable(self, table):
+        self.connect()
+
         try:
             with self.con.cursor() as cur:
                 cur.execute("SELECT * FROM {} LIMIT 0;".format(table))
@@ -46,11 +53,14 @@ class DbClass:
             txt = "n'existe pas."
 
         print("[DATABASE] La table {} {}".format(table, txt))
+        self.deco()
 
         return res
 
 
     def contenuTable(self, table):
+        self.connect()
+
         try:
             with self.con.cursor() as cur:
                 cur.execute("SELECT * FROM {};".format(table))
@@ -59,5 +69,64 @@ class DbClass:
             pass
 
         print("[DATABASE] Récupération contenue table {}".format(table))
+        self.deco()
 
         return res
+
+
+    def supprimerLigne(self, table, condition):
+        self.connect()
+
+        # try:
+        #     with self.con.cursor() as cur:
+        #         cur.execute("DELETE FROM '{}' WHERE '{}'".format(table, condition))
+        # except:
+        #     pass
+
+        with self.con.cursor() as cur:
+            cur.execute("SET FOREIGN_KEY_CHECKS=0")
+            cur.execute("DELETE FROM {} WHERE {}".format(table, condition))
+            cur.execute("SET FOREIGN_KEY_CHECKS=1")
+            self.con.commit()
+
+        print("[DATABASE] Suppression de {} dans la table {}".format(condition, table))
+
+        self.deco()
+
+    def modifierLigne(self, table, donnes, condition):
+        self.connect()
+
+        # try:
+        #     with self.con.cursor() as cur:
+        #         cur.execute("DELETE FROM '{}' WHERE '{}'".format(table, condition))
+        # except:
+        #     pass
+
+        with self.con.cursor() as cur:
+            cur.execute("SET FOREIGN_KEY_CHECKS=0")
+            cur.execute("""UPDATE {} SET {} WHERE {}""".format(table,donnes, condition))
+            cur.execute("SET FOREIGN_KEY_CHECKS=1")
+            self.con.commit()
+
+        print("[DATABASE] Update de {} dans la table {}".format(condition, table))
+
+        self.deco()
+
+    def ajoutLigne(self, table, donnes):
+        self.connect()
+
+        # try:
+        #     with self.con.cursor() as cur:
+        #         cur.execute("DELETE FROM '{}' WHERE '{}'".format(table, condition))
+        # except:
+        #     pass
+
+        with self.con.cursor() as cur:
+            cur.execute("SET FOREIGN_KEY_CHECKS=0")
+            cur.execute("INSERT INTO {} VALUES ({})".format(table,donnes))
+            cur.execute("SET FOREIGN_KEY_CHECKS=1")
+            self.con.commit()
+
+        print("[DATABASE] Insertion de {} dans la table {}".format(donnes, table))
+
+        self.deco()
